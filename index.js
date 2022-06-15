f1648540288445935bec032fe938e9da7277114a
Edward Prado <edward.prado@weareadaptive.com>
2022-06-15 12:43:53 -0400

    refactor: split JS into multiple files

--- index.js
+++ index.js
@@ -161,26 +161,3 @@ function stopStopwatch() {
   $stopStartButton.classList.remove("stop");
   $stopStartButton.classList.add("start");
 }
-
-function formatTime(newestTime, oldTime, timeOffset) {
-  let calcTime = newestTime - timeOffset - oldTime;
-  let minutes = Math.floor(calcTime / 60000);
-  //let seconds = Math.floor(calcTime / 1000);
-  let seconds = Math.floor(calcTime / 1000) % 60;
-  let milliseconds = Math.floor(calcTime % 100);
-
-  //Keep each time segment 2 digits to be consistent with Apple Stopwatch
-  if (minutes < 10) {
-    minutes = "0" + minutes;
-  }
-
-  if (seconds < 10) {
-    seconds = "0" + seconds;
-  }
-
-  if (milliseconds < 10) {
-    milliseconds = "0" + milliseconds;
-  }
-
-  return `${minutes}:${seconds}.${milliseconds}`;
-}
